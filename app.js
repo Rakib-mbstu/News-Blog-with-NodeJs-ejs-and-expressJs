56ae447e81c0b06ba53c23f95e7c1b9a3f6f5ccb
rakib-mbstu <rakibshanto41@gmail.com>
2024-05-02 18:24:18 +0600

    Log in added

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
 
 const connectDB = require("./server/config/db");
+const session = require("express-session");
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 //connect to DB
 
 connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
 
 app.use(express.static("public"));
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,52 +1,74 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
-const User = require('../models/User')
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.jwtSecret;
 
 //admin login
 
-const adminLayout = '../views/layouts/admin';
+const adminLayout = "../views/layouts/admin";
 
 router.get("/admin", async (req, res) => {
-    try {
-      const locals = {
-        title: "Admin",
-        description: "NodeJs bloging site",
-      };
-      res.render('admin/index',{locals, layout:adminLayout})
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  try {
+    const locals = {
+      title: "Admin",
+      description: "NodeJs bloging site",
+    };
+    res.render("admin/index", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-  router.post('/signIn',async(req,res)=>{
-    try{
-      const {userName,password} = req.body;
-      if(req.body.userName==="admin" && req.body.password==='123'){
-        res.redirect('./admin');  
-      }
-      else{
-        res.send("wowo wrong!!!");
-      }
-    }
-    catch(error){
-      console.log(error);
-    }
-  })
+router.get("/dashboard", async (req, res) => {
+  res.render("admin/dashboard");
+});
 
-  router.post('/register',async(req,res)=>{
-    try{
-      const {userName,password} = req.body;
-      if(req.body.userName==="admin" && req.body.password==='123'){
-        res.redirect('./admin');  
-      }
-      else{
-        res.send("wowo wrong!!!");
-      }
+router.post("/sign", async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    console.log(userName + ' ' + password)
+    const user = await User.findOne({ userName });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid" });
     }
-    catch(error){
-      console.log(error);
+    const isPassValid = await bcrypt.compare(password, user.password);
+    if (!isPassValid) {
+      return res.status(401).json({ message: "Invalid" });
     }
-  })
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-module.exports = router
+
+// router.post("/register", async (req, res) => {
+//   try {
+//     const { userName, password } = req.body;
+//     console.log(password);
+//     const hashPassword = await bcrypt.hash(password, 10);
+//     try {
+//       const user = await User.create({
+//         userName: userName,
+//         password: hashPassword,
+//       });
+//       console.log(user);
+//       res.status(201).json({ message: "user created" });
+//     } catch (error) {
+//       console.log(error);
+//       if (error == 11000) {
+//         res.status(409).json({ message: "Already in Use" });
+//       }
+//       res.status(500).json({ message: "Internal Server Error" });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+module.exports = router;
